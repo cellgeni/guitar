434fd71a15ddcb173d1eb152499bad3a351b5acd
Stijn van Dongen <micans@users.noreply.github.com>
2018-06-20 11:49:28 +0100

    remove manual qc, for now

--- bin/irods-iget-runid.sh
+++ bin/irods-iget-runid.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 runid=$1
 lane=$2
 
-imeta qu -z seq -d id_run = $runid and lane = $lane and target = 1 and manual_qc = 1 \
+imeta qu -z seq -d id_run = $runid and lane = $lane and target = 1 \
     | perl -0777 -ne 'while (/collection:\s*(\S+)\ndataObj:\s*(\S+)/gs) { print "iget -K $1/$2\n" }' \
     | bash -e
 
